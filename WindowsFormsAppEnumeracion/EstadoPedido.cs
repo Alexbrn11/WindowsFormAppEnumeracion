6d851ec2d368a45dbbb1eac2102908b5f93d8617
AlexanderTituaña <bryan.crew11@gmail.com>
2025-04-03 22:45:39 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsAppEnumeracion/EstadoPedido.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace WindowsFormsAppEnumeracion
+{
+    public enum EstadoPedido
+
+    {
+        Pendiente, 
+        Procesando, 
+        Enviado, 
+        Entregado, 
+        Cancelado
+    }
+}
